a290061b76a0736ba96b808dfcb20a3d18b6cffb
naveen nelamali <naveen.nelamali@experian.com>
2016-04-12 12:26:50 -0700

    initial file

---
+++ src/main/java/com/test/StandaloneMain.java
@@ -0,0 +1,6 @@
+package com.test;
+public class StandaloneMain{
+	public static void main(String args[]){
+		System.out.println("Execute Heroku standalone main class.. Do what ever you want here...");
+	}
+}
